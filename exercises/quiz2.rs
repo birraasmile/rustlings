5083a3c8251e05ad917fa31de228b0ccf0e18875
birraasmile <birraasmile@outlook.com>
2022-09-21 14:54:08 +0300

    worked about 53 of them so far

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -30,11 +30,12 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String,Command)>) -> Vec<(String,Command)> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<(String,Command)> = vec![];
         for (string, command) in input.iter() {
             // TODO: Complete the function body. You can do it!
+            output.push((string,command));
         }
         output
     }
@@ -43,7 +44,7 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we have to import to have `transformer` in scope?
-    use ???;
+    use my_module::*;
     use super::Command;
 
     #[test]
